ee6bf49eaa2d59485885df0a26e2fd5fe3b3b6ee
jignaciodegiovanni <jignaciodegiovanni@gmail.com>
2022-04-27 01:10:30 -0300

    Changes

--- assets/scripts/pets.js
+++ assets/scripts/pets.js
@@ -4,11 +4,47 @@ $.getJSON("https://cukeds.github.io/anthony/assets/json/pet.json", function(json
   pets = json;
 });
 
+let cellbutton = function(id){
+  rows = document.getElementsByClassName('pet_text');
+  for (let text of rows){
+
+    if (text.id != ""){
+      console.log(text.id);
+    }
+  }
+}
+
 let setup = function(){
 
 
-
-    console.log(pets);
+  let table = document.getElementById("owners_table").getElementsByTagName('tbody')[0];
+  if(pets != undefined){
+    pets.forEach(p =>{
+      let row = table.insertRow();
+      for (let i = 0; i < 3; i++){
+        let cell = row.insertCell();
+        let text;
+        switch(i){
+          case 0:
+            text = document.createTextNode(p.pet_owner);
+            break;
+          case 1:
+            text = document.createTextNode(p.pet_name);
+            break;
+          case 2:
+            text = document.createTextNode(p.pet_type);
+            break;
+        }
+        cell.setAttribute('id', p.id);
+        cell.onclick = function() {cellbutton(cell.id);};
+        cell.appendChild(text);
+      }
+      console.log(p);
+    });
+  }
+  else{
+    setTimeout(() => {setup();}, 2)
+  }
 
 }
 
